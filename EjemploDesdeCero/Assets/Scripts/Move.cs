eb5f40cadbbd94fdf42ed120dee1121957aefa59
alex2e <a.santillana@aol.com>
2018-01-09 10:22:42 +0100

    Física de la esfera

--- EjemploDesdeCero/Assets/Scripts/Move.cs
+++ EjemploDesdeCero/Assets/Scripts/Move.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class Move : MonoBehaviour {
 
     public float speed = 1f;
+    Rigidbody rigidbody;
+    public float forceValue = 0.1f;
 
 	// Use this for initialization
 	void Start () {
-		
+        rigidbody = GetComponent<Rigidbody>(); //Cogemos la referencia al Rigibody	
 	}
 	
-	// Update is called once per frame
+	//Se usa para el movimiento cinematico, una vez por frame.
 	void Update () {
-        transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime); //Imput devuelve un valor entre -1 y 1 dependiendo de la tecla pulsada para un eje.
+        //transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime); //Imput devuelve un valor entre -1 y 1 dependiendo de la tecla pulsada para un eje.
 	}
+
+    //Se usa para el movimiento fisico, no depende de los fps
+    void FixedUpdate() 
+    {
+        rigidbody.AddForce(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * forceValue);
+    }
 }
