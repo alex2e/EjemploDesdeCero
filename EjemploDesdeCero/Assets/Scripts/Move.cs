91e7a3ac8740efbf5cfdfb81dbdcbcd4f119c365
alex2e <a.santillana@aol.com>
2018-01-09 09:26:00 +0100

    Movimiento en eje X e Y.

--- EjemploDesdeCero/Assets/Scripts/Move.cs
+++ EjemploDesdeCero/Assets/Scripts/Move.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Move : MonoBehaviour {
 
+    public float speed = 1f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,6 @@ public class Move : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime); //Imput devuelve un valor entre -1 y 1 dependiendo de la tecla pulsada para un eje.
 	}
 }
