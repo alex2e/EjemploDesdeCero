505adc466e6afe314e6610bb5ee52a5d4fddff35
DESKTOP-7U6IS1N\Alejandro <a.santillana@aol.com>
2018-01-15 10:17:56 +0100

    Colision con sonido, triggers, etc

--- EjemploDesdeCero/Assets/Scripts/Move.cs
+++ EjemploDesdeCero/Assets/Scripts/Move.cs
@@ -8,12 +8,15 @@ public class Move : MonoBehaviour
     public float jumpvalue = 0.1f;
     Rigidbody rigidbody;
     private AudioSource audiosource;
+    private GameObject capsules;
+    public GameObject prefab;
 
     // Use this for initialization
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
         audiosource = GetComponent<AudioSource>();
+        capsules = GameObject.Find("Capsules"); //Para buscarlo por nombre 
     }
 
     // Para los movimientos cinemáticos, se actualiza por cada frame.
@@ -30,17 +33,55 @@ public class Move : MonoBehaviour
     //Se utiliza para las fuerzas físicas.
     private void FixedUpdate()
     {
-        rigidbody.AddForce(new Vector3(Input.GetAxis("Horizontal"), 0
-            , Input.GetAxis("Vertical")) * forcevalue);
+        rigidbody.AddForce(new Vector3(Input.GetAxis("Horizontal"), 0 , Input.GetAxis("Vertical")) * forcevalue);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         //Si ha chocado con un cubo...
-        if (collision.gameObject.tag == "Cubo")
+        if (collision.gameObject.tag == "Cube")
         {
             print("Colision con cubo");
             Destroy(collision.gameObject);
         }
+
+        //Si ha chocado con una capsula...
+        if (collision.gameObject.tag == "Capsule")
+        {
+            collision.gameObject.GetComponent<MeshRenderer>().material.color *= 1.2f;
+            //collision.gameObject.GetComponent<MeshRenderer>().material.SetColor("Negro", new Color(0.2F, 0.3F, 0.4F, 0.5F));
+            collision.gameObject.GetComponent<AudioSource>().Play();
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        //Si ha dejado de chocar con un cubo...
+        if (collision.gameObject.tag == "Cube")
+        {
+           //TODO
+        }
+
+        //Si ha chocado con una capsula...
+        if (collision.gameObject.tag == "Capsule")
+        {
+            collision.gameObject.GetComponent<MeshRenderer>().material.color /= 1.2f;
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        foreach (Transform child in capsules.GetComponentInChildren<Transform>())
+        {
+            if (child.gameObject.tag.Equals("Capsule"))
+            {
+                child.gameObject.GetComponent<MeshRenderer>().enabled = true;
+                child.gameObject.GetComponent<CapsuleCollider>().enabled = true;
+            }
+        }
+
+        //Crea capsula aleatoria
+        Instantiate(prefab, new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f)), Quaternion.identity);
     }
 }
